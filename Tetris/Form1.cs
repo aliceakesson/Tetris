4f9dbabfa50ca6b751c3229ae073d106168f273d
Alice Åkesson <alceakesson@outlook.com>
2022-09-06 14:54:42 +0200

    Added outer wall and basic function structure

--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -12,9 +12,60 @@ namespace Tetris
 {
     public partial class Form1 : Form
     {
+
+        bool gameOver = false; 
+
+        int amountOfBlocks = 10; //??
+
         public Form1()
         {
             InitializeComponent();
+
+            
         }
+
+        //Update
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        void SpawnBlock(int type)
+        {
+            switch(type)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break; 
+                default:
+                    Console.WriteLine("Error in code in SpawnBlock()");
+                    break; 
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down)
+            {
+                //move active block down
+            }
+            else if(e.KeyCode == Keys.Left)
+            {
+                Console.WriteLine("test");
+            }
+            else if(e.KeyCode == Keys.Right)
+            {
+
+            }
+            else if(e.KeyCode== Keys.Up)
+            {
+                //switch block (extra for later)
+            }  
+        }
+
+
     }
 }
--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -18,5 +18,7 @@ namespace Tetris
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+
     }
 }
